25eddf469149eb77dd69c9d8165c557b5f5ded8a
IZALEU <steel.princess@gmail.com>
2022-08-07 23:55:44 -0700

    Update config to static, remove functions

--- src/firebase.js
+++ src/firebase.js
@@ -53,15 +53,3 @@ export async function checkSignIn () {
       });
     });
   }
-
-// const user = auth.currentUser;
-
-// if (user !== null) {
-//   user.providerData.forEach((profile) => {
-//     console.log("Sign-in provider: " + profile.providerId);
-//     console.log("  Provider-specific UID: " + profile.uid);
-//     console.log("  Name: " + profile.displayName);
-//     console.log("  Email: " + profile.email);
-//     console.log("  Photo URL: " + profile.photoURL);
-//   });
-// }
