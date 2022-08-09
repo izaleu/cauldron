26b9810d95eddb7fb2aefc7222b1b8a551aaf48b
IZALEU <steel.princess@gmail.com>
2022-08-08 19:02:36 -0700

    MenuBar formatting

--- src/components/MenuBar.svelte
+++ src/components/MenuBar.svelte
@@ -1,11 +1,16 @@
 <script>
-    let isProd = process.env.NODE_ENV !== 'development'; 
+	let isProd = process.env.NODE_ENV !== 'development';
 </script>
-<div class="menu" class:isProd={isProd}>
-	<div class="logoLink"><a href="/"> {isProd ? 'meal.support' : 'cauldron (dev mode)'}</a></div>
+
+<div class="menu" class:isProd>
+	<div class="logoLink">
+        <a href="/"> {isProd ? 'meal.support' : 'cauldron (dev mode)'}</a>
+    </div>
 	<div class="navBar">
-		<a class="navLink" href="/">Home</a> <a class="navLink" href="/schedule">Schedule</a>
-		<a class="navLink" href="/recipes">Recipes</a> <a class="navLink" href="/login">Log In</a>
+		<a class="navLink" href="/">Home</a>
+		<a class="navLink" href="/schedule">Schedule</a>
+		<a class="navLink" href="/recipes">Recipes</a>
+		<a class="navLink" href="/login">Log In</a>
 	</div>
 </div>
 
@@ -18,9 +23,9 @@
 		grid-column: 5 / 9;
 		justify-self: end;
 	}
-    .isProd {
-        color: red;
-    }
+	.isProd {
+		color: red;
+	}
 	.navLink {
 	}
 	.logoLink {
