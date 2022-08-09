0d57fa8ecfc3f3a59fdc456d2fce90a2f9d3df89
IZALEU <steel.princess@gmail.com>
2022-08-08 18:51:58 -0700

    Add prod/not-prod badge to MenuBar

--- src/components/MenuBar.svelte
+++ src/components/MenuBar.svelte
@@ -1,23 +1,28 @@
-<style>
-    .menu {
-        display: grid;
-        grid: auto / repeat(8, 1fr);
-    }
-    .navBar {
-        grid-column: 5 / 9;
-        justify-self: end;
-    }
-    .navLink {
-
-    }
-    .logoLink {
-        
-    }
-</style>
-
-<div class="menu">
-    <div class="logoLink"><a href="/">Cauldron</a></div>
-    <div class="navBar">
-        <a class="navLink" href="/">Home</a> <a class="navLink" href="/schedule">Schedule</a>  <a class="navLink" href="/recipes">Recipes</a>  <a class="navLink" href="/login">Log In</a>
-    </div>
+<script>
+    let isProd = process.env.NODE_ENV !== 'development'; 
+</script>
+<div class="menu" class:isProd={isProd}>
+	<div class="logoLink"><a href="/"> {isProd ? 'meal.support' : 'cauldron (dev mode)'}</a></div>
+	<div class="navBar">
+		<a class="navLink" href="/">Home</a> <a class="navLink" href="/schedule">Schedule</a>
+		<a class="navLink" href="/recipes">Recipes</a> <a class="navLink" href="/login">Log In</a>
+	</div>
 </div>
+
+<style>
+	.menu {
+		display: grid;
+		grid: auto / repeat(8, 1fr);
+	}
+	.navBar {
+		grid-column: 5 / 9;
+		justify-self: end;
+	}
+    .isProd {
+        color: red;
+    }
+	.navLink {
+	}
+	.logoLink {
+	}
+</style>
